72ef2be9eccf5a1def409053ff821fdbad71fca8
Rahul <rahulsm.1503@gmail.com>
2023-06-25 13:50:42 +0530

    final config

--- page.py
+++ page.py
@@ -8,7 +8,6 @@ from keras.models import load_model
 model = load_model('./Model/model.h5')
 
 df=pd.read_csv('./insurance.csv')
-# st.dataframe(df)
 
 preprocessor = ColumnTransformer(
     transformers=[
@@ -57,32 +56,63 @@ def main():
         if prediction >= income / 12:
             st.write("The applicant is likely to not be able to pay for the insurance")
 
-    st.markdown("#### BMI stats")
-    st.write("BMI: ",bmi)
-    if bmi >=18.5 and bmi<=24.9:
-        st.write("Your BMI falls in the healthy weight range")
-    elif bmi>=25.0 and bmi<=29.9:
-        st.write("Your BMI falls in the overweight range")
-    elif bmi>=30.0:
-        st.write("Your BMI falls in the obese weight range")
-    elif bmi<18.5:
-        st.write("Your BMI falls in the underweight range")
 
-    st.markdown("#### Learn how these factors that affect your premium \n  ##### Smoking")  
+    st.markdown("### Learn how these factors that affect your premium \n  #### Smoking")  
 
     smoker_data=df.groupby('smoker')['charges'].mean()
     st.bar_chart(smoker_data)
     smoker_average=df[df['smoker']=="yes"]['charges'].mean()
     non_smoker_average=df[df['smoker']=="no"]['charges'].mean()
 
-    st.write("Premiums for smokers are on average {:,.2f}".format(1+(smoker_average-non_smoker_average)/smoker_average),"times higher than non-smokers")    
+    st.write("* Premiums for smokers are on average {:,.2f}".format(1+(smoker_average-non_smoker_average)/smoker_average),"times higher than non-smokers")    
     st.markdown("#### BMI")
     bmi_data=df.groupby('bmi')['charges'].mean()
     st.bar_chart(bmi_data)
+
+    st.write("Your BMI: {:,.2f}".format(bmi))
+    mean_bmi=df['bmi'].mean()
+    high_bmi=df[df['bmi']>mean_bmi]['charges'].mean()
+    low_bmi=df[df['bmi']<mean_bmi]['charges'].mean()
+    
+    
+    st.write("* Average BMI : {:,.2f}".format(mean_bmi))
+
+    if bmi >=18.5 and bmi<=24.9:
+        st.write("* Your BMI falls in the healthy weight range")
+    elif bmi>=25.0 and bmi<=29.9:
+        st.write("* Your BMI falls in the overweight range")
+    elif bmi>=30.0:
+        st.write("* Your BMI falls in the obese weight range")
+    elif bmi<18.5:
+        st.write("* Your BMI falls in the underweight range")
+
+    st.write("* Premiums for people with BMI higher than the average BMI are {:,.2f}".format(1+(high_bmi-low_bmi)/high_bmi),"times higher than people with average or below average BMI")    
+
+
     
     st.markdown("#### Region")
     region_data=df.groupby('region')['charges'].mean()
     st.bar_chart(region_data)
     
+    southWestMean=df[df['region']=='southwest']['charges'].mean()
+    southEastMean=df[df['region']=='southeast']['charges'].mean()
+    northWestMean=df[df['region']=='northwest']['charges'].mean()
+    northEastMean=df[df['region']=='northeast']['charges'].mean()
+    countryMean=df['charges'].mean()
+    charges={southWestMean:'Southwest',southEastMean:'Southeast',northWestMean:'Northwest',northEastMean:'Northeast'}
+
+    charge_list=charges.keys()
+
+    charge_max=max(charges.keys())
+
+    highestRegion=max(charge_list)  
+
+    st.write("* National average: ₹{:,.2f}  ".format(countryMean),
+        "\n * Southeast average: ₹{:,.2f}".format(charge_max),
+        "\n * Northeast average: ₹{:,.2f}".format(northEastMean),
+        "\n * Northwest average: ₹{:,.2f}".format(northWestMean),
+        "\n * Southwest average: ₹{:,.2f}".format(southWestMean),
+        "\n * Region with the highest average fees: ",charges.get(highestRegion),
+        )
 if __name__ == '__main__':
     main()
